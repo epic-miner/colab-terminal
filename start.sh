f8462254ceea090bd817a70c9b002460c61bac6d
epic miner <155073729+epic-miner@users.noreply.github.com>
2024-09-06 19:58:14 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -8,11 +8,6 @@ pip install langchain
 pip install langchain-core
 pip install langchain-community
 pip install colab-xterm
-
-# Create or append to Jupyter startup configuration to load colabxterm
-# This adds `%load_ext colabxterm` to the IPython startup file so it runs automatically when a notebook starts.
-mkdir -p ~/.ipython/profile_default/startup
-echo "%load_ext colabxterm" >> ~/.ipython/profile_default/startup/00-colabxterm.py
-
+load_ext colabxterm
 # Optionally, you can start Jupyter Notebook after this if you want
 # jupyter notebook
